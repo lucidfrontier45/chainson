575aae5b96b6481c2600d36d6acfe6dd80c8d3ed
Shiqiao Du <lucidfrontier.45@gmail.com>
2016-02-22 18:03:19 +0900

    added source files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Chainson',
+    version='0.1',
+    packages=['chainson'],
+    url='',
+    license='Apache License 2.0',
+    author='Shiqiao Du',
+    author_email='lucidfrontier.45@gmail.com',
+    description='Chainable JSON Encoder'
+)
